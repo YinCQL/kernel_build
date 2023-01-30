912b682fe6926e599a579f7fff5bee83f9f3112d
YinCQL <71113782+YinCQL@users.noreply.github.com>
2023-01-30 21:58:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 echo "===================Setup Export========================="
 export ARCH=arm64
+export DTC_EXT=dtc
 export CROSS_COMPILE=$GITHUB_WORKSPACE/kernel/tool/aarch/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel/tool/arm/bin/arm-linux-androideabi-
 
